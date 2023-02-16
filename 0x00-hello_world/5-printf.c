a2c06a7ed8b3392ff7a05b0182c4c433b2e55871
sani2023 <hassanabdousani2020@gmail.com>
2023-02-16 13:41:08 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -A program that print a line using the printf function
+ * Return : 0 (Success)
+ */
+int main(void)
+{
+printf("With proper grammar. but the outcome is piece of ard,\n")
+return (0);
+}

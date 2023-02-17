61b80af2daacace235cb75b8f2f41ae4c3d5a2f5
sani2023 <hassanabdousani2020@gmail.com>
2023-02-16 16:31:38 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 #include <unistd.h>
 /**
  * main - A c program that prints a line to the standard error
- * Return: 1 (success)
+ * Return: 1 (Success)
  */
-int main(voit)
+int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);

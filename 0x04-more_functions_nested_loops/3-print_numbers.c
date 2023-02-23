f5aa575493d5e924d162db052adb7c17d02d6005
sani2023 <hassanabdousani2020@gmail.com>
2023-02-23 13:29:25 -0800

    Print num

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - function that print from 0 - 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 0 ; c <= 9 ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}

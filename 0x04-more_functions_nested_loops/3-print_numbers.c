e5b492fb660e674fc056b4f0b5b4813528a3b521
sani2023 <hassanabdousani2020@gmail.com>
2023-02-24 11:18:52 -0800

    print nmbr

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_numbers - function that print from 0 - 9
  *
@@ -10,7 +11,7 @@ void print_numbers(void)
 
 	for (c = '0' ; c <= '9' ; c++)
 	{
-		_putchar('c');
+		_putchar(c);
 	}
 	_putchar('\n');
 }

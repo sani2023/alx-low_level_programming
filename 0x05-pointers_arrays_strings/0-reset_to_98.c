5cf9ec6938b4fb5f88719e1355935772e68f6733
sani2023 <hassanabdousani2020@gmail.com>
2023-02-28 11:07:39 -0800

    Function take pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - tasks a pointerto an int and updates
+ * the value it point to t 98
+ *
+ * @n: An integer input  pointer
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Pointers, String, Arrays

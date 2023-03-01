cbebc94d67467170be2a3cb151ee09dd1367b558
sani2023 <hassanabdousani2020@gmail.com>
2023-03-01 12:01:23 -0800

    Encode rot13

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,6 +10,11 @@ char *string_toupper(char *);
 char *cap_string(char *str);
 char *leet(char *);
 int _putchar(char c);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
 int main();
 
 

fd147539bc57864e85d7028af6907ee8c1c2ea9c
sani2023 <hassanabdousani2020@gmail.com>
2023-03-01 01:08:40 -0800

    header files

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * *_strcat - function that concatenaate a string
+ * @dest: parameter
+ * @src: parameter
+ * Return: always 0
+ */
+char *_strcat(char *dest, char *src)
+{
+
+}

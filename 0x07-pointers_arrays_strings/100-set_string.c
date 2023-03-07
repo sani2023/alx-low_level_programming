81df4b6734ab24afab68e3af59d31b86c32e4fb1
sani2023 <hassanabdousani2020@gmail.com>
2023-03-07 07:36:33 -0800

    convert p to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - set a value of a pointer to a char
+ * @s: parameter
+ * @to: pointer to char
+ *
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

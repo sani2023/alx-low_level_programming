cb78cea86b2ed8235ec3487eefd446a3237959c1
sani2023 <hassanabdousani2020@gmail.com>
2023-03-08 05:35:54 -0800

    print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that print a string followed by a new line
+ * @s: input
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+
+}

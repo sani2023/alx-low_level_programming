b39646acdbb1c6237a9f12c2024b08421261fa39
sani2023 <hassanabdousani2020@gmail.com>
2023-03-14 16:09:13 -0700

    GRe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -25,7 +25,7 @@ char *argstostr(int ac, char **av)
 	str = malloc(sizeof(char) * (len + 1));
 	if (str == NULL)
 		return (NULL);
-	K = 0;
+	k = 0;
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)

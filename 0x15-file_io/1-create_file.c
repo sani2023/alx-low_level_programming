50c8c9540a6120880870959e622e078c791e179d
sani2023 <hassanabdousani2020@gmail.com>
2023-10-30 21:57:58 -0700

    updl

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,11 +25,11 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
-	
+
 	if (fd == -1 || w == -1)
 	return (-1);
 
 	close(fd);
 
 	return (1);
-}	
+}

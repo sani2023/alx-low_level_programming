f82ec685bdb9468be2bf760e502377f950bb0c9d
sani2023 <hassanabdousani2023@gmail.com>
2023-12-07 18:17:30 -0800

    dlsistin len

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - fn return nbr of elemnt
+ * @h: head of node
+ * Return: nbr of node
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}

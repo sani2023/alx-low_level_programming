40545ad12dd5e9967e353bf6f9c41cc9ce4da2f9
sani2023 <hassanabdousani2023@gmail.com>
2023-12-07 20:55:40 -0800

    insert

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct dlistint_s - doubly linked lists
@@ -27,6 +28,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-
+size_t dlistint_len(const dlistint_t *h);
 
 #endif
